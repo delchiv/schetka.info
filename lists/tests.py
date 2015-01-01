32febc76a6c1559a5a8ab6235dd9ecd0acb995cb
delchiv <ismms@yandex.ru>
2015-01-01 22:26:22 +0200

    Первая вьюха возвращает минимальный html

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from .views import home_page
 
 # Create your tests here.
@@ -9,3 +10,10 @@ class TestTest(TestCase):
     def test_root_resolvers_to_home_page_view(self):
         url = resolve('/')
         self.assertEqual(url.func, home_page)
+
+    def test_home_page_returns_valid_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
