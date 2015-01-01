cd9db02ab02e8658fdb5b7ceeb51e6c6683de2d4
delchiv <ismms@yandex.ru>
2015-01-01 22:09:41 +0200

    Первый работающий unit-test. Добавлен url '/;

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from .views import home_page
 
 # Create your tests here.
 
 class TestTest(TestCase):
 
-    def test_simple(self):
-        self.assertEqual(1, 2)
+    def test_root_resolvers_to_home_page_view(self):
+        url = resolve('/')
+        self.assertEqual(url.func, home_page)
