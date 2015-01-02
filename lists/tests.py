42c7e789c3f4ff98ba1231a164baeda7e74491a7
delchiv <ismms@yandex.ru>
2015-01-02 22:03:19 +0200

    Добавлен редирект после POSTа. В шаблоне выводится несколько значений

--- lists/tests.py
+++ lists/tests.py
@@ -20,16 +20,41 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_accept_POST_method(self):
+    def test_home_page_can_save_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
-        self.assertEqual(expected_html, response.content.decode())
+        self.assertEqual(Item.objects.count(), 1)
+        item = Item.objects.first()
+        self.assertEqual(item.text, 'A new list item')
+
+    def test_home_page_can_redirect_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_items(self):
+        Item.objects.create(text='Item 1')
+        Item.objects.create(text='Item 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('Item 1', response.content.decode())
+        self.assertIn('Item 2', response.content.decode())
+
+    def test_home_page_saves_items_only_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
