f6b6ec7bc705b376b67c97b9ce271dbe534ebd9a
delchiv <ismms@yandex.ru>
2015-01-02 20:09:14 +0200

    Добавлена таблица Items в базу данных

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         # Эдит услышала о новом хорошем онлайновом to-do листе.
         # Решила проверить его (листа) домашнюю страницу
@@ -32,9 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # Когда она нажала Enter, страница обновилась. Теперь в списке значится
         # "1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # На странице также осталось поле, для ввода следующего задания.
         # Эдит ввела "Use peacock feathers to make a fly" (она очень методична)
@@ -43,9 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Эдит стало интересно, запомнит ли сайт ее список. Затем она обратила внимание,
         # что сайт сгенерировал уникальный URL для ее списка.
--- lists/tests.py
+++ lists/tests.py
@@ -2,11 +2,13 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+
 from .views import home_page
+from .models import Item
 
 # Create your tests here.
 
-class TestTest(TestCase):
+class HomePageTest(TestCase):
 
     def test_root_resolvers_to_home_page_view(self):
         url = resolve('/')
@@ -28,3 +30,23 @@ class TestTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
         self.assertEqual(expected_html, response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        item = Item()
+        item.text = 'First item'
+        item.save()
+       
+        item = Item()
+        item.text = 'Second item'
+        item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'First item')
+        self.assertEqual(second_saved_item.text, 'Second item')
